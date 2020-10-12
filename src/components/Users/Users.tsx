53e02e7e12b05d4fec09edce38ec33919a094866
1 <a1@MacBook-Pro-1.local>
2020-10-12 13:41:29 +0300

    TypeScript files added

---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,41 @@
+import React, {FC} from "react";
+import Paginator from "../common/Paginator/Paginator";
+import User from "./User";
+import {UserType} from "../../types/types";
+
+type PropsType = {
+    totalUsersCount: number
+    pageSize: number
+    currentPage: number
+    onPageChanged: (pageNumber: number) => void
+    users: Array<UserType>
+    followingInProgress: Array<number>
+    unfollow: (userId: number) => void
+    follow: (userId: number) => void
+}
+
+let Users: FC<PropsType> = ({
+                                currentPage,
+                                totalUsersCount,
+                                pageSize,
+                                onPageChanged,
+                                users,
+                                ...props
+                            }) => {
+
+    return <div>
+        <Paginator totalItemsCount={totalUsersCount} pageSize={pageSize} currentPage={currentPage}
+                   onPageChanged={onPageChanged}/>
+        <div>
+            {
+                users.map(u => <User user={u}
+                                     followingInProgress={props.followingInProgress}
+                                     key={u.id}
+                                     unfollow={props.unfollow}
+                                     follow={props.follow}/>)
+            }
+        </div>
+    </div>
+};
+
+export default Users;
