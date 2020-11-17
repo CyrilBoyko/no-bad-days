ce579af815cf151b2b044ffc6b6c9f33ddc36df4
1 <a1@MacBook-Pro-1.local>
2020-11-17 15:13:06 +0200

    Updates

---
+++ precache-manifest.f8d9a755fdbc00d8e09bd19cfb685975.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6bccffdb686f394cedfefc2ed4bd7f0",
+    "url": "/no-bad-days/index.html"
+  },
+  {
+    "revision": "44db342b881d369a33f2",
+    "url": "/no-bad-days/static/css/3.b98278eb.chunk.css"
+  },
+  {
+    "revision": "9a5a59e0f5b4771a8a55",
+    "url": "/no-bad-days/static/css/4.691d462b.chunk.css"
+  },
+  {
+    "revision": "0cf877f6bf999fa2658d",
+    "url": "/no-bad-days/static/css/main.7e19c28e.chunk.css"
+  },
+  {
+    "revision": "5980f5bf95f5b6b105e6",
+    "url": "/no-bad-days/static/js/2.803737a8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/no-bad-days/static/js/2.803737a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44db342b881d369a33f2",
+    "url": "/no-bad-days/static/js/3.de52f54a.chunk.js"
+  },
+  {
+    "revision": "9a5a59e0f5b4771a8a55",
+    "url": "/no-bad-days/static/js/4.a1b0d5c7.chunk.js"
+  },
+  {
+    "revision": "0cf877f6bf999fa2658d",
+    "url": "/no-bad-days/static/js/main.23c21886.chunk.js"
+  },
+  {
+    "revision": "c61f5463b7cf7f8b747b",
+    "url": "/no-bad-days/static/js/runtime-main.3f18649c.js"
+  },
+  {
+    "revision": "5c8e984d827f77d34cea51fd19831f5f",
+    "url": "/no-bad-days/static/media/beach.5c8e984d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/no-bad-days/precache-manifest.f2dadd641f911a0284f845bc5511d7c5.js"
+  "/no-bad-days/precache-manifest.f8d9a755fdbc00d8e09bd19cfb685975.js"
 );
 
 self.addEventListener('message', (event) => {
